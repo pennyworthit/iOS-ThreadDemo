bdbbcb12578819ba8867ba70476659bca1efb8ef
Monsoir <monwingyeung@gmail.com>
2018-04-09 11:09:25 +0800

    What: 1.创建

---
+++ ThreadingDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThreadingDemo
+//
+//  Created by Christopher on 2018/4/9.
+//  Copyright © 2018 wenyongyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
